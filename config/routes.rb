c6badf06b64a18fb00b2570b6678e9890b89905c
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-05-15 11:31:25 -0700

    add root index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'my_tree/create'
   get 'my_tree/edit'
   get 'my_tree/update'
+  root 'my_tree#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
